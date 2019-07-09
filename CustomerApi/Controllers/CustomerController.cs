849f8770f8d6e09826e96d9c2195ee373e90612c
aslihan <kocabasasli66@gmail.com>
2019-07-09 16:25:43 +0300

    This repository includes routing microservices on the .net core using the api gateway.Ocelot library was used as api gateway.
    This repository includes routing microservices on the .net core using the api gateway.Ocelot library was used as api gateway.

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/Customer
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Customer 1", "Customer 2" };
+        }
+    }
+}
